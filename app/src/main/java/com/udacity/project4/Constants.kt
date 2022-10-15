d3f076a5fad17516cb73fe5b39065fb01179cf16
abanoub.samirfahmi@gmail.com <abanoub.samirfahmi@gmail.com>
2022-10-15 21:25:28 +0200

    Add a geofencing request implementation 1-	Declare android.os.Build.VERSION  to ensure that the app has permission to run in the foreground and background 2-	determine whether permission has been granted or not 3-	check Request background and fine location permissions 4-	check  the gadget’s location. 5-	Adding Geofence You’ll need a method to inherit from the PendingIntent to manage Geofence transitions. 6-	Declare  PendingIntent object that  describes both an intent and the action that should be done in response to it  and used for a BroadcastReceiver to control the Geofence transitions. Save the reminder to the local db 1-	Declare instance reminderDataItem of ReminderDataItem class and set properties   {title , description , location , latitude , longitude} 2-	Store into Local DB through passing  locationData after Adding Geofence successfully to view model  and execute _viewModel.validateAndSaveReminder(reminderDataItem)
    the implementation of the reminder details
    1-	Declare reminderData object  by using intent.extras?  keyword to get extra data
    2-	Binding data to layout (save reminder )

--- app/src/main/java/com/udacity/project4/Constants.kt
+++ app/src/main/java/com/udacity/project4/Constants.kt
@@ -22,4 +22,22 @@ object Constants {
 
         // used in  requestLocationPermission
         const val PERMISSION_CODE_LOCATION_REQUEST = 1
+
+        // used in requestForegroundAndBackgroundLocationPermissions API to save reminder fragment
+        const val REQUEST_FOREGROUND_AND_BACKGROUND_PERMISSION_RESULT_CODE = 33
+        const val REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE = 34
+        const val TAG_save = "SaveReminderFragment"
+
+        // used in checkDeviceLocationSetting
+        const val REQUEST_TURN_DEVICE_LOCATION_ON = 29
+
+        //used in  addGeoForRemainder  >> save reminder fragment
+
+        const val GEOFENCE_RADIUS_IN_METERS = 100f
+        const val NEVER_EXPIRES = Geofence.NEVER_EXPIRE
+
+        // used in  onRequestPermissionsResult
+        const val LOCATION_PERMISSION_INDEX = 0
+        const val BACKGROUND_LOCATION_PERMISSION_INDEX = 1
+
 }
